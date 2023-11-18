5f0b15551a173d42907634381ffcaa57eb3d7a19
wansie-boop <136750877+wansie-boop@users.noreply.github.com>
2023-11-18 21:27:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function nav() {
+    document.getElementById("menu-icon").classList.toggle("menu-icon-open");
+    document.getElementById("close-icon").classList.toggle("close-icon-open");
+
+    document.getElementById("nav").classList.toggle("open");
+}
